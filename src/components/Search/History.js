66b2ab48a41695d3a830718db2dfb3ed5b7f4d4c
kong19950928@163.com <kong19950928@163.com>
2019-09-16 23:16:42 +0800

    搜索静态

---
+++ src/components/Search/History.js
@@ -0,0 +1,20 @@
+import React from 'react'
+class History extends React.Component{
+    render() {
+        return(
+            <div className={"history"}>
+                <p>
+                    <i>历史纪录</i>
+                    <i className={"iconfont iconlajixiang"}></i>
+                </p>
+                <p>
+                    <b>去年夏天啊</b>
+                    <b>去年夏天啊</b>
+                    <b>去年夏天啊</b>
+                    <b>去年夏天啊</b>
+                </p>
+            </div>
+        )
+    }
+}
+export default  History
