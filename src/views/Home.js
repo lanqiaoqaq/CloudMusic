8a2c800b7028978db42304c6a0b094c22be40106
shaokangnan <285836964@qq.com>
2019-09-18 11:07:48 +0800

    抽屉

--- src/views/Home.js
+++ src/views/Home.js
@@ -10,16 +10,18 @@ import Profile from './Profile'
 import Find from './Find'
 import Trend from "./Trend";
 import AudioPlayer from '../components/AudioPlayer'
+import Drawer from '../components/Drawer_my.js'
 class App extends React.Component{
     render(){
         return (
             <>
             {/*<Router>*/}
                 <nav className={"top_nav"}>
-                    <span className={"sidebar"}>
-                        <i className={"iconfont icon-sangeheng"}></i>
-                        <b className={"msgNum"}>3</b>
-                    </span>
+                    {/*<span className={"sidebar"}>*/}
+                        {/*<i className={"iconfont icon-sangeheng"}></i>*/}
+                        {/*<b className={"msgNum"}>3</b>*/}
+                    {/*</span>*/}
+                    <Drawer></Drawer>
                     <NavLink to={"/profile"}>我的</NavLink>
                     <NavLink to={"/"} exact>发现</NavLink>
                     <NavLink to={"/trend"}>动态</NavLink>
